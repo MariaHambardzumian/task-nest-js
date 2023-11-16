5466aceed493c9194c2c8e2e06126356fc8a9b11
MariaH <maria_hambardzumyan@epam.com>
2023-11-16 17:33:10 +0400

    usecase of Interceptor

--- src/ToDo/todo.controller.ts
+++ src/ToDo/todo.controller.ts
@@ -6,6 +6,7 @@ import {
   Delete,
   Param,
   Body,
+  UseInterceptors,
 } from '@nestjs/common';
 import {
   ApiTags,
@@ -18,6 +19,7 @@ import { TodoService } from './todo.service';
 import { TodoDto } from './dto/todo.model';
 import { LoggerService } from 'src/logger/logger.service';
 import { LogMessages } from 'src/logger/log-messages.enum';
+import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
 
 @Controller('todos')
 @ApiTags('Todos')
@@ -37,26 +39,10 @@ export class TodoController {
     description: 'Retrieves a Todo lists',
     type: [TodoDto],
   })
+  @UseInterceptors(LoggingInterceptor)
   async getAllTodos(): Promise<TodoDto[]> {
-    try {
-      this.logger.log('-Sending request- ' + LogMessages.GET_ALL_TODOS, true);
-
-      const todos = await this.todoService.getAllTodos();
-
-      this.logger.log(
-        '-Received response for- ' + LogMessages.GET_ALL_TODOS,
-        false,
-        todos,
-      );
-
-      return todos;
-    } catch (error) {
-      this.logger.error(
-        `${LogMessages.ERROR_GET_ALL_TODOS}: ${error.message}`,
-        error.stack,
-      );
-      throw error;
-    }
+    const todos = await this.todoService.getAllTodos();
+    return todos;
   }
 
   // get with id
@@ -71,31 +57,14 @@ export class TodoController {
     description: 'Retrieves a specific Todo by ID.',
     type: TodoDto,
   })
+  @UseInterceptors(LoggingInterceptor)
   async getTodoById(@Param('id') id: string): Promise<TodoDto> {
-    try {
-      if (!id.trim()) {
-        this.logger.warn(LogMessages.MISSING_ID_WARNING);
-      }
-
-      const logMessage = `-Sending request- ${LogMessages.GET_TODO_BY_ID}`;
-      this.logger.log(logMessage, true, id);
-
-      const todo = await this.todoService.getTodoById(id);
-
-      this.logger.log(
-        `-Received response for- ${LogMessages.GET_TODO_BY_ID} : ${id}`,
-        false,
-        todo,
-      );
-
-      return todo;
-    } catch (error) {
-      this.logger.error(
-        `${LogMessages.ERROR_GET_TODO_BY_ID} : ${id} \nMessage: ${error.message}`,
-        error.stack,
-      );
-      throw error;
+    if (!id.trim()) {
+      this.logger.warn(LogMessages.MISSING_ID_WARNING);
     }
+    const todo = await this.todoService.getTodoById(id);
+
+    return todo;
   }
 
   // create new
@@ -106,36 +75,20 @@ export class TodoController {
     description: 'Adds a new Todo to the list.',
   })
   @ApiResponse({ status: 201, description: 'Creates a new Todo.' })
+  @UseInterceptors(LoggingInterceptor)
   async addTodo(@Body() todo: TodoDto): Promise<TodoDto> {
-    try {
-      if (
-        !todo ||
-        !todo.description ||
-        typeof todo.done !== 'boolean' ||
-        !todo.id
-      ) {
-        this.logger.warn(LogMessages.MISSING_FIELDS_AUTOFILL_WARNING);
-      }
-
-      const logMessage = `-Sending request- ${LogMessages.ADD_TODO}`;
-      this.logger.log(logMessage, true, todo);
-
-      const addedTodo = await this.todoService.addTodo(todo);
-
-      this.logger.log(
-        `-Received response for- ${LogMessages.ADD_TODO}`,
-        false,
-        addedTodo,
-      );
-
-      return addedTodo;
-    } catch (error) {
-      this.logger.error(
-        `${LogMessages.ERROR_ADD_TODO}: ${error.message}`,
-        error.stack,
-      );
-      throw error;
+    if (
+      !todo ||
+      !todo.description ||
+      typeof todo.done !== 'boolean' ||
+      !todo.id
+    ) {
+      this.logger.warn(LogMessages.MISSING_FIELDS_AUTOFILL_WARNING);
     }
+
+    const addedTodo = await this.todoService.addTodo(todo);
+
+    return addedTodo;
   }
 
   // update
@@ -147,33 +100,20 @@ export class TodoController {
     description: ' Updates a specific Todo identified by its ID.',
   })
   @ApiResponse({ status: 200, description: 'Updates a specific Todo by ID.' })
+  @UseInterceptors(LoggingInterceptor)
   async updateTodo(
     @Param('id') id: string,
-    @Body() updatedTodo: TodoDto,
+    @Body() updatedTodo: Partial<TodoDto>,
   ): Promise<TodoDto> {
-    try {
-      if (!updatedTodo || !Object.keys(updatedTodo).length) {
-        this.logger.warn(LogMessages.NO_DATA_WARNING);
-      } else {
-        const logMessage = `-Sending request- ${LogMessages.UPDATE_TODO}: ${id}`;
-        this.logger.log(logMessage, true, updatedTodo);
+    if (!updatedTodo || !Object.keys(updatedTodo).length) {
+      this.logger.warn(LogMessages.NO_DATA_WARNING);
+    } else {
+      const updatedTodoResult = await this.todoService.updateTodo(
+        id,
+        updatedTodo,
+      );
 
-        const updatedTodoResult = await this.todoService.updateTodo(
-          id,
-          updatedTodo,
-        );
-
-        this.logger.log(
-          `-Received response for- ${LogMessages.UPDATE_TODO}: ${id}`,
-          false,
-          updatedTodoResult,
-        );
-
-        return updatedTodoResult;
-      }
-    } catch (error) {
-      this.logger.error(`${LogMessages.ERROR_UPDATE_TODO}: ${id}`, error.stack);
-      throw error;
+      return updatedTodoResult;
     }
   }
 
@@ -185,20 +125,8 @@ export class TodoController {
     description: ' Removes a specific Todo identified by its ID.',
   })
   @ApiResponse({ status: 204, description: 'Deletes a specific Todo by ID.' })
+  @UseInterceptors(LoggingInterceptor)
   async deleteTodo(@Param('id') id: string): Promise<void> {
-    try {
-      const logMessage = `-Sending request- ${LogMessages.DELETE_TODO}: ${id}`;
-      this.logger.log(logMessage, true);
-
-      await this.todoService.deleteTodo(id);
-
-      this.logger.log(
-        `-Received response for- ${LogMessages.DELETE_TODO}: ${id}`,
-        false,
-      );
-    } catch (error) {
-      this.logger.error(`${LogMessages.ERROR_DELETE_TODO}: ${id}`, error.stack);
-      throw error;
-    }
+    await this.todoService.deleteTodo(id);
   }
 }
